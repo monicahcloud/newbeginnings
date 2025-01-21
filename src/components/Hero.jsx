44d08b171247602a843470e7335a0827006a2424
Monicah Cloud <monicahcloud@gmail.com>
2025-01-21 17:35:12 -0600

    working on hero and nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,14 @@ const Hero = () => {
       id="home"
       className="bg-fixed bg-parallax bg-cover flex items-center justify-center h-full"
     >
-      <div className="w-full h-screen flex items-center bg-blackOverlay">
+      <div className="w-full h-screen flex items-end md:items-center bg-blackOverlay">
         <div className="container mx-auto mb-40">
-          <h1 className="text-white text-5xl max-w-2xl uppercase font-bold  md:text-7xl ">
+          <h1 className="text-white text-4xl max-w-2xl uppercase font-bold  md:text-7xl ">
             New Beginnings Outreach WorldWide Mininstries
           </h1>
-          <p className="mt-2 text-3xl text-slate-100 capitalize tracking-wide"></p>
+          <p className="mt-2  text-2xl md:text-3xl text-slate-100 font-semibold capitalize tracking-wide">
+            Where Faith Restores, Hope Rebuilds, and Lives Begin Anew.
+          </p>
         </div>
       </div>
     </section>
