d15e570588e1cf585d7633fa11006cf137b5b9e9
Monicah Cloud <monicahcloud@gmail.com>
2025-01-21 22:06:42 -0600

    events added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="bg-fixed bg-parallax bg-cover  flex items-center justify-center h-full"
+      className="bg-fixed bg-parallax bg-cover flex sm:h-1/2 h-full"
     >
-      <div className="w-full h-screen flex justify-center items-center">
-        <div className="container mx-auto mb-40">
-          <h1 className="text-white text-4xl w-full uppercase font-bold  md:text-7xl ">
+      <div className="w-full h-screen flex justify-center text-center items-center">
+        <div className="container  mb-40">
+          <h1 className="text-white text-4xl w-full  uppercase font-bold  md:text-7xl ">
             New Beginnings Outreach WorldWide Mininstries
           </h1>
           <p className="mt-2  text-2xl md:text-3xl text-slate-100 font-semibold capitalize tracking-wide">
