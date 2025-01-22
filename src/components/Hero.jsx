f672b1ba8f6056722a20168973a08536fbe0bb61
Monicah Cloud <monicahcloud@gmail.com>
2025-01-21 22:48:14 -0600

    events added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     >
       <div className="w-full h-screen flex justify-center text-center items-center">
         <div className=" mb-0">
-          <h1 className="text-white text-6xl font-serif w-full uppercase font-bold md:text-8xl">
+          <h1 className="text-white text-5xl font-serif w-full uppercase font-bold md:text-8xl">
             New Beginnings Outreach WorldWide Mininstries
           </h1>
           <p className="mt-2  text-3xl md:text-3xl text-slate-100 font-semibold capitalize tracking-wide">
